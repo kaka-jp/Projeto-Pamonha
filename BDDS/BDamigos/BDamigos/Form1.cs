ad43f11bdc6ad639c82fdac57bf6e80c5447a7b8
kakak <kazuo.iwata@protonmail.com>
2019-07-31 15:29:52 -0300

    kaka

---
+++ BDDS/BDamigos/BDamigos/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BDamigos
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection mCon;
+        private MySqlDataAdapter mAdapter;
+        private DataSet mDs;
+
+    
+        public Form1()
+        {
+            InitializeComponent();
+        mDs = new DataSet();
+        mCon = new MySqlConnection("Persist Security Info=false;" +
+            "server =127.0.0.1;database=bdamigos;uid=root;Password=1234");
+
+        mCon.Open();
+        exibe();
+
+        }
+        public void exibe()
+        {
+            if (mCon.State == ConnectionState.Open)
+            {
+                mDs.Clear();
+                mAdapter = new MySqlDataAdapter("Select * FROM tbl_amigos", mCon);
+                mAdapter.Fill(mDs, "dados");
+                DataGridView1.DataSource = null;
+                DataGridView1.Rows.Clear();
+                DataGridView1.Refresh();
+                DataGridView1.DataSource = mDs;
+                DataGridView1.DataMember = "dados";
+            }
+        }
+        public void limpa()
+        {
+            txtNome.Clear();
+            txtEnd.Clear();
+            txtCidade.Clear();
+            txtIdade.Clear();
+            
+        }
+
+        private void Cadastrar_Click(object sender, EventArgs e)
+        {
+            MySqlCommand SQL = new MySqlCommand("insert into tbl_amigos(amg_nome,amg_end,amg_cidade,amg_idade) values (@amgnome,@amgend,@amgcidade,@amgidade)", mCon);
+
+            SQL.Parameters.AddWithValue("@amgnome", txtNome.Text);
+            SQL.Parameters.AddWithValue("@amgend", txtEnd.Text);
+            SQL.Parameters.AddWithValue("@amgcidade", txtCidade.Text);
+            SQL.Parameters.AddWithValue("@amgidade", txtIdade.Text);
+           
+            SQL.ExecuteNonQuery();
+
+            MessageBox.Show("cadastro realizado com sucesso");
+            exibe();
+            limpa();
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtResponsavel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIdade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCidade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEnd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BDDS/BDamigos/BDamigos/Form2.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace BDamigos
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.btnVizu = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 83);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BUSCAR CONTATO";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblNome);
+            this.groupBox1.Controls.Add(this.lblCPF);
+            this.groupBox1.Controls.Add(this.lblTelefone);
+            this.groupBox1.Controls.Add(this.lblEmail);
+            this.groupBox1.Location = new System.Drawing.Point(67, 206);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(229, 161);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados de contato";
+            // 
+            // lblNome
+            // 
+            this.lblNome.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNome.Location = new System.Drawing.Point(20, 31);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(180, 18);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCPF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCPF.Location = new System.Drawing.Point(20, 125);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(180, 18);
+            this.lblCPF.TabIndex = 6;
+            this.lblCPF.Text = "CPF";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTelefone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTelefone.Location = new System.Drawing.Point(20, 61);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(180, 18);
+            this.lblTelefone.TabIndex = 4;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEmail.Location = new System.Drawing.Point(20, 93);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(180, 18);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // btnVizu
+            // 
+            this.btnVizu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVizu.Location = new System.Drawing.Point(244, 130);
+            this.btnVizu.Name = "btnVizu";
+            this.btnVizu.Size = new System.Drawing.Size(137, 36);
+            this.btnVizu.TabIndex = 6;
+            this.btnVizu.Text = "Visualizar";
+            this.btnVizu.UseVisualStyleBackColor = true;
+            this.btnVizu.Click += new System.EventHandler(this.btnVizu_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(397, 130);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(137, 36);
+            this.btnExcluir.TabIndex = 7;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlterar.Location = new System.Drawing.Point(397, 331);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(137, 36);
+            this.btnAlterar.TabIndex = 8;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(67, 141);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnVizu);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button btnVizu;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+   
+    }
+}
