b04358e0b6df36beef8a9bd5b5e19c035555b72b
kazuo <kazuo.iwata@protonmail.com>
2019-08-26 15:17:57 -0300

    novo

--- PC/net_beans/Matrizes/src/challengeM/Desafio_9.java
+++ PC/net_beans/Matrizes/src/challengeM/Desafio_9.java
@@ -13,18 +13,20 @@ import javax.swing.JOptionPane;
  */
 public class Desafio_9 {
     public static void main(String[] args) {
-       int mat[][] = new int [4][7];
-       int i,j;
+       String Prateleira[][] = new String [4][7];
+       String Lista []= {"AEROFÓLIO","ALTERNADOR","AMORTECEDOR","AR-CONDICIONADO","BARRA ESTABILIZADORA","BATERIA","BOBINA","BOMBA DE ÁGUA","BOMBA DE COMBUSTÍVEL","CÂMBIO AUTOMÁTICO","CÂMBIO MANUAL","CARBURADOR","CILÍNDROS DE VÁLVULAS","CINTO DE SEGURANÇA","COMANDO DE VÁLVULAS","CORREIA DENTADA","DIFERENCIAL","EMBREAGEM","FREIOS","IGNIÇÃO ELETRÔNICA","INJEÇÃO ELETRÔNICA","JUNTA DO CABEÇOTE","JUNTA HOMOCINÉTICA","MOTOR DE ARRANQUE","PNEU","RADIADOR","SUSPENSÃO","PARACHOQUE"};
+       int i,j,h=0;
        String x = "";
        
-       for (i=0; i < 6; i++){
-           for (j=0; j < 3; j++){
-               mat[i][j]= (int) (Math.random() * 100);          
-               x+= mat[i][j]+",";
+       for (i=0; i < 4; i++){
+           for (j=0; j < 7; j++){
+               h=(int)(Math.random() * 27);
+               Prateleira[i][j]= Lista[h];          
+               x+= Prateleira[i][j]+",";
            }
            x+= "\n";
        }
-        JOptionPane.showMessageDialog(null,"A matriz é \n"+x);
+        JOptionPane.showMessageDialog(null,"A Prateleira ficaria assim \n"+x);
        
     }
     
