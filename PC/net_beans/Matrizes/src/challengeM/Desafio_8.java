f02e1932bbe37cc489465897a1be91212cbac25a
kazuo <kazuo.iwata@protonmail.com>
2019-08-26 14:42:35 -0300

    novo

--- PC/net_beans/Matrizes/src/challengeM/Desafio_8.java
+++ PC/net_beans/Matrizes/src/challengeM/Desafio_8.java
@@ -5,10 +5,32 @@
  */
 package challengeM;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Aluno
  */
 public class Desafio_8 {
+    public static void main(String[] args) {
+          
+       int D[][] = new int [3][3];
+       int i,j,n=0,maior=0;
+       String x = "";
+       for (i=0; i<=2; i++){
+           for (j=0; j<=2; j++){
+               D[i][j]= (int) (Math.random() * 50);
+               x+=D[i][j]+",";
+               n=D[i][j];
+               if(n < maior){
+                   maior = D[i][j];
+               }
+           }
+           
+           x+="\n";
+           
+       }
+        JOptionPane.showMessageDialog(null,"O maior numero é:"+n+"\nA matriz é \n"+x);
     
+    }
 }
