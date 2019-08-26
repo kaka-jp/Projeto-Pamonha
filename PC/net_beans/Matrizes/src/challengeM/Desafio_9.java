6723a3fac5a2c506c01fb16350fa4dc60ecaeddb
Aluno <Aluno@LAB3M10>
2019-08-26 14:45:33 -0300

    26/08/19

--- PC/net_beans/Matrizes/src/challengeM/Desafio_9.java
+++ PC/net_beans/Matrizes/src/challengeM/Desafio_9.java
@@ -5,10 +5,27 @@
  */
 package challengeM;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Aluno
  */
 public class Desafio_9 {
+    public static void main(String[] args) {
+       int mat[][] = new int [4][7];
+       int i,j;
+       String x = "";
+       
+       for (i=0; i < 6; i++){
+           for (j=0; j < 3; j++){
+               mat[i][j]= (int) (Math.random() * 100);          
+               x+= mat[i][j]+",";
+           }
+           x+= "\n";
+       }
+        JOptionPane.showMessageDialog(null,"A matriz é \n"+x);
+       
+    }
     
 }
