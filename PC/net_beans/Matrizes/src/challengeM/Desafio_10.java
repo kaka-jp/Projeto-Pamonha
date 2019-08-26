79b962c69c15e8b55713f7d34c9f6ccb0e417cba
kazuo <kazuo.iwata@protonmail.com>
2019-08-26 15:29:08 -0300

    novo

--- PC/net_beans/Matrizes/src/challengeM/Desafio_10.java
+++ PC/net_beans/Matrizes/src/challengeM/Desafio_10.java
@@ -5,10 +5,32 @@
  */
 package challengeM;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Aluno
  */
 public class Desafio_10 {
+    public static void main(String[] args) {
+       
+       String Resposta [] = {"d","b","c","a","a","d","e","c","a","d"};
+       int i,j,h=0,n=0;
+       String x = ""; 
+       
+       n = Integer.parseInt(JOptionPane.showInputDialog("Entre com a quantidade de Linhas da Matriz"));
+       
+       String Prateleira[][] = new String [n][10];
+       
+       for (i=0; i < n; i++){
+           for (j=0; j < 10; j++){
+               Prateleira[i][j]= Resposta[n];          
+               x+= Prateleira[i][j]+",";
+           }
+           x+= "\n";
+       }
+        JOptionPane.showMessageDialog(null,"A Prateleira ficaria assim \n"+x);
+       
+    }
     
 }
