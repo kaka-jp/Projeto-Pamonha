bf00236f9ada7cca4152991fbc3bc721e9e1b843
kazuo <kazuo.iwata@protonmail.com>
2019-08-26 14:17:46 -0300

    novo

--- PC/net_beans/Matrizes/src/challengeM/Desafio_6.java
+++ PC/net_beans/Matrizes/src/challengeM/Desafio_6.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -35,38 +35,3 @@ public class Desafio_6 {
         JOptionPane.showMessageDialog(null,"A Média da Matriz é:"+t+"\nA matriz é \n"+x);
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package challengeM;
-
-import javax.swing.JOptionPane;
-
-/**
- *
- * @author Aluno
- */
-public class Desafio_6 {
-    public static void main(String[] args) {        
-       int i,j,n=0,z,y;
-       String x = "";
-       
-       z = Integer.parseInt(JOptionPane.showInputDialog("Entre com a quantidade de Linhas da Matriz"));
-       y = Integer.parseInt(JOptionPane.showInputDialog("Entre com a quantidade de Colunas da Matriz"));
-       
-       int C[][] = new int [z][y];
-       for (i=0; i<z; i++){
-           for (j=0; j<y; j++){
-               C[i][j]= (int) (Math.random() *(20));
-               x+=C[i][j]+",";
-               n+=C[i][j];
-           }
-           x+="\n";
-       }
-        JOptionPane.showMessageDialog(null,"A Somatória da Matriz é:"+n+"\nA matriz é \n"+x);
-    }
-}
->>>>>>> b7373617e362ae1d0393b1f0f064e6a47c4a366c
