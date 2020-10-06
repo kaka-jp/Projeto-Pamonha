5c17d28db5a60f8e136f42062b1383db3f0152f4
kaka-jp <52668182+kaka-jp@users.noreply.github.com>
2020-10-06 13:17:24 -0300

    Erro arrumado do discord
    Prosseguir para parte 3

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var mineflayer = require('mineflayer')
 var D = require('discord.js')
 var client = new D.Client();
 
+
+let ip = "CantinhodoWifi.aternos.me";
+let username = "Pamonha";
+let ver = "1.15.2";
 var bot = mineflayer.createBot({
-  host: 'CantinhodoWifi.aternos.me', // optional
-  username: 'Pamonha', // email and password are required only for
-  version: false                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
+  host: ip,
+  username: username,
+  version: ver
 })
 
 client.on('ready', async () => {
   console.log(`Discord Bot on. Loggined as ${client.user.tag}`)
-})
+})  
 
 bot.on('login', async () => {
   console.log(`bot is on At ${ip}`)
@@ -19,15 +23,11 @@ bot.on('login', async () => {
 
 
 bot.on("message", message => {
-  let channel = client.channels.cache.get('715203209333047306')
+  var channel = client.channels.cache.get('763069270845947944')
   if (!channel) return;
   channel.send(`From Server Chat >> ${message}`)
 
 })
-
-
-
-
 
 
 client.login('NzYzMDM5OTg5Nzc2NTE1MTAy.X3x6Xg.tbBjhdmS0djs7JnrN8mjNEpUI3c')
