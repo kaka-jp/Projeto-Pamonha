0c514f27afaa1b0b511cefd9eec11382626e3f1f
kaka-jp <52668182+kaka-jp@users.noreply.github.com>
2020-10-06 12:23:44 -0300

    Discord log tá funcionando
    Só arrumar os demais erros que tá dando por ai

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const mineflayer = require('mineflayer')
+var mineflayer = require('mineflayer')
+var D = require('discord.js')
+var client = new D.Client();
 
-const bot = mineflayer.createBot({
+var bot = mineflayer.createBot({
   host: 'CantinhodoWifi.aternos.me', // optional
-  port: 25565,       // optional
   username: 'Pamonha', // email and password are required only for
   version: false                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
 })
 
-bot.on('chat', function (username, message) {
-  if (username === bot.username) return
-  bot.chat(message)
+client.on('ready', async () => {
+  console.log(`Discord Bot on. Loggined as ${client.user.tag}`)
 })
 
-// Log errors and kick reasons:
-bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
-bot.on('error', err => console.log(err))
+bot.on('login', async () => {
+  console.log(`bot is on At ${ip}`)
+})
+
+
+
+bot.on("message", message => {
+  let channel = client.channels.cache.get('715203209333047306')
+  if (!channel) return;
+  channel.send(`From Server Chat >> ${message}`)
+
+})
+
+
+
+
+
+
+client.login('NzYzMDM5OTg5Nzc2NTE1MTAy.X3x6Xg.tbBjhdmS0djs7JnrN8mjNEpUI3c')
+.catch(error => {
+  console.log('cant login');
+})
