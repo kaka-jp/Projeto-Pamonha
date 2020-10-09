503978e7728e78213d27af040f0fb5e772d6bac0
kaka-jp <52668182+kaka-jp@users.noreply.github.com>
2020-10-08 23:32:13 -0300

    Mudança do config.json

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const config = require("./config.json")
 
 const bot = mineflayer.createBot({
   host: config.minecraft.serverIP, // optional
+  port: 25565,
   username: acc.minecraft.Username, // email and password are required only for
   version: false                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
 })
 
 bot.on("login", () => {
     console.log(`Logged in as ${bot.username} \nServer: ${config.minecraft.serverIP}\n\n`)
-    bot.chat (`${config.minecraft}`)
+    bot.chat (`${config.minecraft.joinCMD}`)
 
 })  
 
