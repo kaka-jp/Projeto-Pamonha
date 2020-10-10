0cb9529169861f5c3eda9341d86716714f3df95e
kazuo <52668182+kaka-jp@users.noreply.github.com>
2020-10-10 12:07:24 -0300

    Arrumando o index.js

--- index.js
+++ index.js
@@ -3,58 +3,47 @@ const mineflayer = require("mineflayer");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-<<<<<<< HEAD
-//let prefix = ".";
-
-let token = config.token;
 
 
 let sending = false;
 let chatData = []
-=======
-let prefix = ".";
->>>>>>> parent of 948154b... login está pronto (index.js)
 
 let bot = mineflayer.createBot({
-    host: `${config.serverip}`,
-    port: `${config.port}`,
-    username: `${config.username}`,
     version: "1.15.2",
-<<<<<<< HEAD
-=======
-    host: "hub.dev-urandom.eu",
-    username: "Pamonha",
->>>>>>> parent of 948154b... login está pronto (index.js)
+    host: "to.play-ml.ru",
+    //host: "hub.dev-urandom.eu",
+    //username: "Pamonha",
+    //host:"localhost",
+    //port: 49518,
+    username: "Kurihara_P",
 })
 
 
 
 client.on("ready", async => {
     console.log("Bot Online")
+    bot.chat("/login biluteteia")
 })
 
-bot.on("login", async => {
+bot.on("ready", async => {
     console.log("Ingame Bot Online")
-    bot.chat("/register")
 })
 
 
 bot.on("message", message => {
-<<<<<<< HEAD
+
     if(sending == true) {
         chatData.push(`${message}`)
     }
 
 
-=======
->>>>>>> parent of 948154b... login está pronto (index.js)
     let channel = client.channels.cache.get("763069270845947944")
     if (!channel) return;
     
     channel.send(`${message}`)
 })
 
-<<<<<<< HEAD
+
 client.on("message", async msg => {
     let args = msg.content.split(" ").slice(1)
 
@@ -85,7 +74,4 @@ client.on("message", async msg => {
 
 })
 
-client.login(token)
-=======
 client.login("NzYzMDM5OTg5Nzc2NTE1MTAy.X3x6Xg.tbBjhdmS0djs7JnrN8mjNEpUI3c")
->>>>>>> parent of 948154b... login está pronto (index.js)
